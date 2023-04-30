c52c90e71f81e553f088d8a5b17d709e3e12b4a2
kayal5169 <baieluranov777@gmail.com>
2023-04-30 17:39:43 +0600

    feat: added second language with second HTML

--- script.js
+++ script.js
@@ -12,11 +12,19 @@ let right = document.querySelector(".right");
 let up = document.querySelector(".up");
 let down = document.querySelector(".down");
 let caps = document.querySelector(".caps");
+let backspace = document.querySelector(".backspace");
+let ralt = document.querySelector(".ralt")
+let lalt = document.querySelector(".lalt")
+let enter = document.querySelector(".enter")
+let win = document.querySelector(".win")
+let link = document.querySelector(".lang-lnk")
 
 for( let i = 0;i < keys.length; i++){
     keys[i].setAttribute('key', keys[i].innerText)
     keys[i].setAttribute('UpperCase', keys[i].innerText.toUpperCase())
 }
+
+    
 
 window.addEventListener("keydown", function(e){
     for( let i = 0;i < keys.length; i++){
@@ -70,15 +78,16 @@ window.addEventListener("keyup", function(e){
         if(e.code == "Space"){
             space.classList.remove("active")
         }
-        if(e.code == "CapsLock"){
-            caps.classList.remove("active")
-        }
+        
         if(e.code == "ShiftLeft"){
             shift.classList.remove("active")
         }
         if(e.code == "Tab"){
             tab.classList.remove("active");
-            
+        
+        }
+        if(e.code == "CapsLock"){
+            caps.classList.remove("active")
         }
         if(e.code == "ArrowUp"){
             up.classList.remove("active")
@@ -104,6 +113,38 @@ window.addEventListener("keyup", function(e){
         
     }
 })
+backspace.addEventListener("click", () => {
+    let value = text.innerText;
+    value = value.slice(0, -1);
+    text.innerText = value;
+  });
 
+window.addEventListener("click", function(e){   
+    let key = e.target
+    if(key.hasAttribute("key") && key != backspace 
+    && key != caps && key != shift 
+    && key != tab && key != ralt 
+    && key != lalt && key != enter 
+    && key !=lctrl && key !=rctrl 
+    && key != up && key != down 
+    && key !=right && key !=left 
+    && key != del && key !=win
+    && key != space){
+    text.innerText += key.getAttribute("key");
+    key.classList.add("active");
+    this.setTimeout(()=>{
+        key.classList.remove("active")
+    },200)
+
+    }
+function delClass(){
+    key.classList.remove("active")
+}
+})
+document.addEventListener('keydown', function(event) {
+    if (event.altKey && event.key === 's') {
+    link.click();
+    }
+  });
 
 
